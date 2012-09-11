1244880accf6d37a523fa758d77667dd09b77538
DaSourcerer <webmaster@dasourcerer.net>
2012-09-11 09:36:21 +0200

    Added a check + fallback for deflate-encoded content. See habari/habari#365

--- framework/web/CHttpClient.php
+++ framework/web/CHttpClient.php
@@ -551,7 +551,12 @@ class CHttpClientConnector extends CComponent
 					$response->body=bzdecompress($response->body);
 					break;
 				case 'deflate':
-					$response->body=gzuncompress($response->body);
+                    // Is this really DEFLATE? Some servers seem to advertise RFC 1952 encoded data here, so let's check
+                    // for a zlib header first.
+                    if(ord($response->body[0]) == 0x78 && in_array(ord($response->body[1]), array(0x01, 0x5e, 0x9c, 0xda)))
+					    $response->body=gzuncompress($response->body);
+                    else
+                        $response->body=gzdecode($response->body);
 					break;
 				case 'identity';
 					break;
